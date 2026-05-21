72222e55f3db31c35766ee3c73cb5d38b1f67755
Chris <chris.frazier@searchspring.com>
2026-05-21 10:30:56 -0700

    refactor(testing): testing

--- packages/snap-preact-demo/templates/src/index.tsx
+++ packages/snap-preact-demo/templates/src/index.tsx
@@ -44,8 +44,59 @@ let templatesConfig: SnapTemplatesConfig = {
 		style: globalStyles,
 		overrides: {
 			default: {
-				facet: {
-					// iconColor: 'red'
+				// 'autocompleteFixed results': {
+				// 	columns: 3,
+				// 	rows: 2,
+				// },
+				searchCollapsible: {
+					// hideTopToolbar: true,
+					hideMiddleToolbar: true,
+				},
+				'searchCollapsible toolbar.top': {
+					layout: [['_', 'searchHeader', '_'], ['banner.header'], ['button.sidebar-toggle', '_', 'paginationInfo', '_', 'sortBy'], ['banner.banner']],
+				},
+				'searchCollapsible results': {
+					columns: 3,
+				},
+				pagination: {
+					pages: 3,
+				},
+				paginationInfo: {
+					infoText: (data) => `We found ${data.pagination?.totalResults} results`,
+				},
+				'button.sidebar-toggle': {
+					icon: false,
+					content: (
+						<a href="#" className="toggle-btn utility-bar__item toggle-btn--revealed-desktop" data-toggle-filters="">
+							<span className="button-icon">
+								<svg width="23" height="19" viewBox="0 0 20 20" stroke-width="1.25" fill="none" xmlns="http://www.w3.org/2000/svg">
+									<line x1="1" y1="6" x2="19" y2="6" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></line>
+									<line x1="1" y1="14" x2="19" y2="14" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></line>
+									<circle cx="7" cy="6" r="3" fill="none" stroke="currentColor"></circle>
+									<circle cx="13" cy="14" r="3" fill="none" stroke="currentColor"></circle>
+								</svg>
+							</span>
+							<span>Filter</span>
+
+							<span className="toggle-btn__chevron">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="24"
+									height="24"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="1.3"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									className="feather feather-chevron-right"
+								>
+									<title>Right</title>
+									<polyline points="9 18 15 12 9 6"></polyline>
+								</svg>
+							</span>
+						</a>
+					),
 				},
 			},
 		},
@@ -71,7 +122,7 @@ let templatesConfig: SnapTemplatesConfig = {
 		targets: [
 			{
 				selector: '#athos-layout',
-				component: 'Search',
+				component: 'SearchCollapsible',
 			},
 		],
 		settings: {
